10dfe3cd68651e38afed4ecb42c4d6de57674e13
Khaleeq <khaleeqjava@gmail.com>
2021-01-29 09:36:06 -0500

    Second commit with change third change

--- git_from_eclipse/src/main/java/com/khaleeqjava/git_from_eclipse/App.java
+++ git_from_eclipse/src/main/java/com/khaleeqjava/git_from_eclipse/App.java
@@ -4,10 +4,8 @@ package com.khaleeqjava.git_from_eclipse;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World! + first change from java khaleeq+ third commit");
+	}
 }
